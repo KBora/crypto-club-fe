945d77ef5fccd54aa35e937bc0db378753e319a3
wookkii <wookkii@home>
2021-04-05 12:31:01 +1000

    Add profile information

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <LoginButton></LoginButton>
         <LogoutButton></LogoutButton>
       </header>
+      <h3>Profile information</h3>
+      <Profile></Profile>
     </div>
   );
 }
